5b8087dad11b1982a4a4134d3cd281cb52f54d5f
viviaan09 <ngvivian682@gmail.com>
2025-11-29 16:43:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 import api from "./route/api.js"
 import database from "./config/database.js"
 import cors from "cors"
-
+ 
 const app = express();
 
 app.use(express.json());
